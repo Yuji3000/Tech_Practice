ad6bdd3f6d49d1e7122e284a1754476598db8559
Yuji Kosakowski <yuji.kosakowski@gmail.com>
2023-07-24 13:35:46 -0600

    Create new file

---
+++ leetcode/two_sum.rb
@@ -0,0 +1,28 @@
+# https://leetcode.com/problems/two-sum/
+
+# Example 1:
+# Input: nums = [2,7,11,15], target = 9
+# Output: [0,1]
+# Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+# Example 2:
+# Input: nums = [3,2,4], target = 6
+# Output: [1,2]
+
+# Example 3:
+# Input: nums = [3,3], target = 6
+# Output: [0,1]
+
+
+#Find the two elements in the array that equal the target
+#only two elements will fulfil this, cannot use same element twice
+
+def two_sum(nums, target)
+  nums.each do |num|
+    
+  end
+end
+
+nums = [2,7,11,15]
+target = 9
+p two_sum(nums, target)
