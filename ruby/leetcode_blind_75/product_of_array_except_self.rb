8136e899f941e7c6a071506ecaa190186980b31e
Yuji Kosakowski <yuji.kosakowski@gmail.com>
2024-02-08 20:54:11 -0700

    rework leetcode problem

--- ruby/leetcode_blind_75/product_of_array_except_self.rb
+++ ruby/leetcode_blind_75/product_of_array_except_self.rb
@@ -3,20 +3,38 @@
 
 
 
+# def product_except_self(nums)
+#   num_array = []
+#   nums.each do |num|
+#     new_nums = nums.reject do |number| 
+#       number == num
+#     end
+#     if new_nums.empty? == true
+#       new_nums = [0]
+#     end
+#     num_array << new_nums.reject.inject(:*)
+#   end
+#   return num_array
+# end
+
+
+
+
+
+
+
+
+
+
 def product_except_self(nums)
-  num_array = []
+  array = []
   nums.each do |num|
     new_nums = nums.reject do |number| 
       number == num
     end
-    if new_nums.empty? == true
-      new_nums = [0]
-    end
-    num_array << new_nums.reject.inject(:*)
+    array << new_nums.inject(:*)
   end
-  return num_array
 end
-
 nums = [1,2,3,4]
 # Output: [24,12,8,6]
 p product_except_self(nums)
